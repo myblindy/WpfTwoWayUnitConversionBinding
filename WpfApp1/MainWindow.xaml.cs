c80d20428a71c65c928f4fb74efa6d0b5e4daadf
myblindy <tweety.04.01@gmail.com>
2020-02-04 22:30:23 -0500

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ((VMType)DataContext).Length = new UnitValue { Value = 50.12, Unit = "cm" };
+        }
+    }
+
+    class VMType : ReactiveObject
+    {
+        UnitValue length;
+        public UnitValue Length { get => length; set => this.RaiseAndSetIfChanged(ref length, value); }
+    }
+
+    public class UnitValue : ReactiveObject
+    {
+        double value;
+        public double Value { get => value; set => this.RaiseAndSetIfChanged(ref this.value, value); }
+        string unit;
+        public string Unit { get => unit; set => this.RaiseAndSetIfChanged(ref unit, value); }
+    }
+}
---
+++ WpfApp1/UnitValueBindingExtension.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Xaml;
+
+namespace WpfApp1
+{
+    public class UnitValueBindingExtension : MarkupExtension
+    {
+        public PropertyPath Path { get; set; }
+        public string Unit { get; set; } = "m";
+        public BindingMode Mode { get; set; }
+        public string StringFormat { get; set; }
+        public UpdateSourceTrigger UpdateSourceTrigger { get; set; }
+
+        internal string SourceUnit { get; set; }
+
+        public UnitValueBindingExtension() { }
+
+        public UnitValueBindingExtension(PropertyPath path) => Path = path;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var binding = new Binding { Converter = new UnitValueBindingExtensionConverter(this), Path = Path, Mode = Mode, StringFormat = StringFormat, UpdateSourceTrigger = UpdateSourceTrigger };
+            var bindingexpression = (BindingExpression)binding.ProvideValue(serviceProvider);
+
+            var target = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
+            var context = ((FrameworkElement)target.TargetObject).DataContext;
+            var srcPI = GetProperty(Path.Path, context);
+            UnitValue srcObject = null;
+
+            void contextChangeDelegate(object context, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName != srcPI.Name) return;
+
+                if (!(srcObject is null)) srcObject.PropertyChanged -= sourceObjectChangeDelegate;
+
+                srcObject = (UnitValue)srcPI.GetValue(context);
+                if (!(srcObject is null)) { srcObject.PropertyChanged += sourceObjectChangeDelegate; sourceObjectChangeDelegate(srcObject, new PropertyChangedEventArgs("Unit")); }
+            }
+            ((INotifyPropertyChanged)context).PropertyChanged += contextChangeDelegate;
+            contextChangeDelegate(context, new PropertyChangedEventArgs(srcPI.Name));
+
+            void sourceObjectChangeDelegate(object srcObject, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == "Unit") SourceUnit = ((UnitValue)srcObject).Unit;
+                bindingexpression.UpdateTarget();
+            }
+
+            return bindingexpression;
+        }
+
+        public static PropertyInfo GetProperty(string PropertyPath, object obj)
+        {
+            if (string.IsNullOrEmpty(PropertyPath))
+                return null;
+
+            var SourceProperties = PropertyPath.Split('.');
+            var PropertyType = obj.GetType();
+            var PropertyInfo = PropertyType.GetProperty(SourceProperties[0]);
+            PropertyType = PropertyInfo.PropertyType;
+            for (var x = 1; x < SourceProperties.Length; ++x)
+            {
+                PropertyInfo = PropertyType.GetProperty(SourceProperties[x]);
+                PropertyType = PropertyInfo.PropertyType;
+            }
+            return PropertyInfo;
+        }
+    }
+
+    internal class UnitValueBindingExtensionConverter : IValueConverter
+    {
+        private readonly UnitValueBindingExtension UnitValueBindingExtension;
+
+        public UnitValueBindingExtensionConverter(UnitValueBindingExtension unitValueBindingExtension) => UnitValueBindingExtension = unitValueBindingExtension;
+
+        static readonly Dictionary<string, double> MeterMultiplierTable = new Dictionary<string, double>
+        {
+            ["cm"] = 100,
+            ["mm"] = 1000,
+            ["m"] = 1,
+            ["km"] = 0.0001,
+            ["NM"] = 0.000539957,
+        };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+            var unitvalue = (UnitValue)value;
+            return System.Convert.ToDouble(unitvalue.Value) / MeterMultiplierTable[unitvalue.Unit] * MeterMultiplierTable[UnitValueBindingExtension.Unit];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value is null || string.IsNullOrWhiteSpace((string)value) ? null :
+            new UnitValue
+            {
+                Unit = UnitValueBindingExtension.SourceUnit,
+                Value = System.Convert.ToDouble(value) * MeterMultiplierTable[UnitValueBindingExtension.SourceUnit] / MeterMultiplierTable[UnitValueBindingExtension.Unit],
+            };
+    }
+}
